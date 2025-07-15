6e5167606fd9d8d797612ff40fd8d91da3c8f29c
Raul Ruiz <raul.ruiz01@sap.com>
2025-07-15 22:52:54 +0000

    Mini arreglos Eliu

---
+++ app/escalacion/dist/Component-dbg.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/fe/core/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("escalacion.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
